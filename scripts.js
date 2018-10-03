092abaa6f0b2e82c6a6d9424de26366faaa11d1e
Stephen Lang <stephenlang91@gmail.com>
2018-10-02 22:09:40 -0400

    clear and remake button working. Problem with changing colour has arrisen as a result.

--- scripts.js
+++ scripts.js
@@ -1,19 +1,44 @@
+//vars
+var cells = document.querySelectorAll(".gridsquare");
+var clearbutton = document.querySelector("#clear");
+var remakebutton = document.querySelector('#remake');
 var container = document.querySelector("#container");
+
+//creates the grid
 function genGrid(num) {
     for (i=0;i< (num * num);i++){
       cell = document.createElement('div');
-    cell.className = "gridsquare";
-    container.appendChild(cell);
+      cell.className = "gridsquare";
+      container.appendChild(cell);
     }    
   }
   
-genGrid(16);
 
-var cells = document.querySelectorAll(".gridsquare")
-
-cells.forEach((cell) => {
-  cell.addEventListener('mouseenter', (e) => {
-    console.log(cell);
-    cell.className = "active gridsquare";
+//colours the gridsquares
+cells.forEach((square) => {
+  square.addEventListener('mouseenter', (e) => {
+    console.log(square);
+    square.className = "active gridsquare";
   });
 });
+
+//clear button
+clearbutton.addEventListener('click', (e) => {
+cells.forEach((cell) => {
+  console.log(cell);
+  cell.className="gridsquare";
+  }); 
+});
+
+//remakes the grid
+remakebutton.addEventListener('click', (e) => {
+  let gridnum = prompt("Pick a number");
+  var removeCells = document.querySelectorAll(".gridsquare");
+  removeCells.forEach((cell)=> {
+    container.removeChild(cell);
+  }); 
+  document.documentElement.style.setProperty("--gridNum", gridnum);
+  genGrid(gridnum);
+  
+});
+
